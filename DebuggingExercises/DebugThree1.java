014e0f0c5ae7a814f296b04a8bc6cb4fccc877bf
ruixuan98 <ruixuan90@gmail.com>
2022-11-29 15:34:28 +0800

    Debug Programs

--- DebuggingExercises/DebugThree1.java
+++ DebuggingExercises/DebugThree1.java
@@ -6,11 +6,12 @@ public class DebugThree1
       double myCheck = 50.00;
       double yourCheck = 19.95;
       System.out.println("Tips are");
-      calcTip(mycheck);
-      calctip(yourCheck);
+      calcTip(myCheck);
+      calcTip(yourCheck);
     }
-    public static int calcTip(int bill)
+    public static void calcTip(double bill)
     {
+      double tip;
       final double RATE = 0.15;
       tip = bill + RATE;
       System.out.println("The tip should be at least " + tip);
